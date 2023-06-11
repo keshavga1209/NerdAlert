6649d8f02fa2217f56a2bbb0376a5f979b81ed8d
Vidit Pushkarna <viditpushkarna@Vidits-MacBook-Pro.local>
2023-06-11 13:11:59 +0530

    node-server established

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+import routes from "./api/routes/index.js";
+import fileUpload from "express-fileupload";
+
+// fire up the express app
+const app = express();
+
+const PORT = 8081;
+
+const server = http.Server(app);
+
+// app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+// use express router
+app.use("/", routes(io));
+
+server.listen(process.env.PORT || PORT, function (err) {
+    if (err) {
+        console.log("oh no no no no no");
+        return;
+    }
+    console.log("hey there i am using classroom on port : ", PORT);
+});
+
+export { server, app }
+
+// add server self pinging
